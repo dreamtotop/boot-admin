815f4d02e6ce234426fb9e7d91dac7e86ac48f86
top <1390276254@qq.com>
2020-09-10 22:59:06 +0800

    定义统一的数据返回格式，常量字段

---
+++ thyme-boot/src/main/java/org/top/thymeboot/ThymeBootApplication.java
@@ -0,0 +1,13 @@
+package org.top.thymeboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeBootApplication.class, args);
+    }
+
+}
